a4235992a9cfc98e24cf1a4e8ac3379416d46dbb
WhiskeyTea <wingless.void@gmail.com>
2025-10-25 20:44:20 +0300

    Написал возможность рисовать, букашка двигается

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import pygame
 from sys import exit
-
+import numpy as np
+import math
+# pos=np.array([600, 600]), vec=np.array([0, 0])
 
 class Bug(pygame.sprite.Sprite):
-    def __init__(self, pos=(600, 600)):
+    def __init__(self, pos=np.array([600, 600]), speed=1, angle=0):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load('graphics/bug.png').convert_alpha(), 0, 0.05)
+        self.angle = angle
+        self.image = pygame.transform.rotozoom(pygame.image.load('graphics/bug.png').convert_alpha(), math.degrees(self.angle), 0.05)
         self.pos = pos
-        self.rect = self.image.get_rect(midbottom = pos)
+        self.rect = self.image.get_frect(center = pos)
+        self.speed = speed
+        self.vec = np.array([math.cos(self.angle) * speed, math.sin(self.angle) * speed])
 
-    def move(self, x, y):
-        self.pos = (self.pos[0] + x, self.pos[1] + y)
+    def change_dir(self):
+        self.angle += 0.1
+        # self.image = pygame.transform.rotozoom(self.image, math.degrees(self.angle), 1)
+        self.vec = np.array([math.cos(self.angle) * self.speed, math.sin(self.angle) * self.speed])
+
+    def move(self, k=1):
+        self.rect.center += self.vec * k
+
+    def check_future_collision(self, time=10):
+        while pygame.sprite.spritecollide(Bug(self.rect.center + self.vec * time, self.speed, self.angle), lines, False):
+            self.change_dir()
+
+    def update(self):
+        self.check_future_collision()
+        self.move()
+
+
 
 
 class Line(pygame.sprite.Sprite):
@@ -23,7 +43,15 @@ class Line(pygame.sprite.Sprite):
         self.surf = surf
 
     def draw(self):
-        pygame.draw.aaline(self.surf, self.color, self.start, self.end, width=self.width)
+        self.rect = pygame.draw.aaline(self.surf, self.color, self.start, self.end, width=self.width)
+
+
+def crd_sum(x, y):
+    return x[0] + y[0], x[1] + y[1]
+
+
+def check_future_collision(time):
+    return pygame.sprite.spritecollide(Bug(bug.sprite.pos + bug.sprite.vec * time), lines, False)
 
 
 
@@ -31,6 +59,7 @@ pygame.init()
 game_state = 1  # ["start", "active"]
 screen = pygame.display.set_mode((1280, 720))
 screen.fill("gray")
+bg = pygame.transform.scale_by(pygame.image.load('graphics/923c508dac22439e4f56502f7181e040.jpg').convert(), 3)
 pygame.display.set_caption("Букашка")
 clock = pygame.time.Clock()
 
@@ -53,13 +82,15 @@ while True:
                 mouse_pos = pygame.mouse.get_pos()
                 mv = pygame.mouse.get_rel()
                 st = tuple(mouse_pos[i] - mv[i] for i in [0, 1])
-                line = Line(screen, mouse_pos, st, "#000000")
+                line = Line(bg, mouse_pos, st, "#000000")
                 line.draw()
                 lines.add(line)
 
 
     if game_state:
+        screen.blit(bg, (0, 0))
         bug.draw(screen)
+        bug.update()
         mouse_pos = pygame.mouse.get_rel()
     else:
         test_font = pygame.font.Font(None, 60)  # insert '<folder with font>/<filename.ttf>' instead of None
